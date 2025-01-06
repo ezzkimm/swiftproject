708a011cc3fd69322a9f915e8479063ba3ebbcbd
ezzkimm <dmswjd1515@gmail.com>
2025-01-07 00:24:38 +0900

    project init

---
+++ sijosae/sijosae/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sijosae
+//
+//  Created by 김은정 on 1/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sijosae/sijosae/sijosaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  sijosaeApp.swift
+//  sijosae
+//
+//  Created by 김은정 on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct sijosaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
