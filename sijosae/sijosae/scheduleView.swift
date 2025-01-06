82237b334a6add9609e069dc026ee99f6f5cca44
ezzkimm <dmswjd1515@gmail.com>
2025-01-07 01:51:46 +0900

    [FEAT] scheduleView edit : font size, add border to checkbox

--- sijosae/sijosae/scheduleView.swift
+++ sijosae/sijosae/scheduleView.swift
@@ -9,23 +9,24 @@ import SwiftUI
 
 struct scheduleView: View {
     var body: some View {
-        Text("월")
+        Text("월").font(.system(size:50))
             .padding(100)
         VStack(alignment:.leading){
             HStack{
-                Text("쳌박")
+                Text("쳌")
+                    .border(Color.black)
                 Text("밥을먹고잘자기")
             }
             HStack{
-                Text("쳌박")
+                Text("쳌").border(Color.black)
                 Text("출근하기")
             }
             HStack{
-                Text("쳌박")
+                Text("쳌").border(Color.black)
                 Text("안녕은정아처음본순간부터너에게고백하고싶엇어")
             }
             HStack{
-                Text("쳌박")
+                Text("쳌").border(Color.black)
                 Text("너앞에선용기가없어서말로못하겟더라그래서")
             }
         }
