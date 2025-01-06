d823cc69f50bea55dcf645f97de8aa0d3e0dbd59
ezzkimm <dmswjd1515@gmail.com>
2025-01-07 01:02:44 +0900

    file add

---
+++ sijosae/sijosae/weeklyView.swift
@@ -0,0 +1,18 @@
+//
+//  weeklyView.swift
+//  sijosae
+//
+//  Created by 김은정 on 1/7/25.
+//
+
+import SwiftUI
+
+struct weeklyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    weeklyView()
+}
